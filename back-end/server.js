9bca38612c4d52ed93864d92da77e28991d6a41a
MatheusGlanz <glanzmatheus@gmail.com>
2025-05-23 15:56:57 -0300

    correcao deploy novamente

--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path'); // necessário para resolver caminhos de arquivos
 const comprasRoutes = require('./routes/compras');
 const cors = require('cors');
 
@@ -7,9 +8,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Conexão com o MongoDB
 mongoose.connect('mongodb+srv://glanzmatheus:Z3rhVfzBfLheHrcc@cluster0.zhhzmoh.mongodb.net/assistente?retryWrites=true&w=majority');
 
+// Rotas da API
 app.use('/api/compras', comprasRoutes);
+
+// Servir arquivos estáticos do frontend (dist do Vite)
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Redirecionar qualquer rota desconhecida para o index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('Servidor rodando na porta', PORT));
